8cebf089e4a9f55ceb1a89da8d2160102ed5a0aa
Scott Dexter <sdexter@brooklyn.cuny.edu>
2016-09-13 11:12:13 -0400

    Initial commit

---
+++ Unit1RAT/src/Widget.java
@@ -0,0 +1,14 @@
+
+public class Widget {
+	private int i;
+	private String s;
+	public Widget (int i, String s) {
+		this.i = 2*i;
+		this.s = s;
+	}
+	
+	public String widge() {
+		char c = s.charAt(i);
+		return (c + " " + s.replace(c,++c));
+	}
+}
